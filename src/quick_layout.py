37bb249142379a3200f3921fbebd35ccfe27860d
Nikolai Poliarnyi <polarnick@agisoft.com>
2021-01-26 11:57:34 +0300

    quick_layout.py: added support for antenna offset

--- src/quick_layout.py
+++ src/quick_layout.py
@@ -11,13 +11,9 @@ found_major_version = ".".join(ps.app.version.split('.')[:2])
 if found_major_version != compatible_major_version:
     raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
 
-try:
-    from PySide2.QtGui import *
-    from PySide2.QtCore import *
-    from PySide2.QtWidgets import *
-except ImportError:
-    from PySide.QtGui import *
-    from PySide.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
 import copy
 
 
@@ -30,29 +26,6 @@ def time_measure(func):
         return res
 
     return wrapper
-
-
-def delta_vector_to_chunk(v1, v2):
-    chunk = ps.app.document.chunk
-    v1 = chunk.crs.unproject(v1)
-    v2 = chunk.crs.unproject(v2)
-    v1 = chunk.transform.matrix.inv().mulp(v1)
-    v2 = chunk.transform.matrix.inv().mulp(v2)
-    z = v2 - v1
-    z.normalize()
-
-    return z
-
-
-def get_chunk_vectors(lat, lon):
-    z = delta_vector_to_chunk(ps.Vector([lon, lat, 0]), ps.Vector([lon, lat, 1]))
-    y = delta_vector_to_chunk(ps.Vector([lon, lat, 0]), ps.Vector([lon + 0.001, lat, 0]))
-    x = delta_vector_to_chunk(ps.Vector([lon, lat, 0]), ps.Vector([lon, lat + 0.001, 0]))
-    return x, y, -z
-
-
-def wgs_to_chunk(chunk, point):
-    return chunk.transform.matrix.inv().mulp(chunk.crs.unproject(point))
 
 
 def show_message(msg):
@@ -74,37 +47,29 @@ def check_chunk(chunk):
     return True
 
 
-# returns distance estimation between two cameras in chunk
-def get_photos_delta(chunk):
-    mid_idx = int(len(chunk.cameras) / 2)
-    if mid_idx == 0:
-        return ps.Vector([0, 0, 0])
-    c1 = chunk.cameras[:mid_idx][-1]
-    c2 = chunk.cameras[:mid_idx][-2]
-    print(c1.reference.location)
-    print(c2.reference.location)
-    offset = c1.reference.location - c2.reference.location
-    for i in range(len(offset)):
-        offset[i] = math.fabs(offset[i])
-    return offset
+def get_antenna_transform(sensor):
+    location = sensor.antenna.location
+    if location is None:
+        location = sensor.antenna.location_ref
+    rotation = sensor.antenna.rotation
+    if rotation is None:
+        rotation = sensor.antenna.rotation_ref
+    return ps.Matrix.Diag((1, -1, -1, 1)) * ps.Matrix.Translation(location) * ps.Matrix.Rotation(ps.Utils.ypr2mat(rotation))
 
 
-def get_chunk_bounds(chunk):
-    min_latitude = min(c.reference.location[1] for c in chunk.cameras if c.reference.location is not None)
-    max_latitude = max(c.reference.location[1] for c in chunk.cameras if c.reference.location is not None)
-    min_longitude = min(c.reference.location[0] for c in chunk.cameras if c.reference.location is not None)
-    max_longitude = max(c.reference.location[0] for c in chunk.cameras if c.reference.location is not None)
-    offset = get_photos_delta(chunk)
-    offset_factor = 2
-    delta_latitude = offset_factor * offset.y
-    delta_longitude = offset_factor * offset.x
+def init_chunk_transform(chunk):
+    if chunk.transform.scale is not None:
+        return
+    chunk_origin = ps.Vector([0, 0, 0])
+    for c in chunk.cameras:
+        if c.reference.location is None:
+            continue
+        chunk_origin = chunk.crs.unproject(c.reference.location)
+        break
 
-    min_longitude -= delta_longitude
-    max_longitude += delta_longitude
-    min_latitude -= delta_latitude
-    max_latitude += delta_latitude
-
-    return min_latitude, min_longitude, max_latitude, max_longitude
+    chunk.transform.scale = 1
+    chunk.transform.rotation = ps.Matrix.Diag((1, 1, 1))
+    chunk.transform.translation = chunk_origin
 
 
 # Evaluates rotation matrices for cameras that have location
@@ -112,9 +77,7 @@ def get_chunk_bounds(chunk):
 # and yaw is evaluated from current copter direction
 # current direction is evaluated simply subtracting location of
 # current camera from the next camera location
-# i and j are unit axis vectors in chunk coordinate system
-# i || North
-def estimate_rotation_matrices(chunk, i, j):
+def estimate_rotation_matrices(chunk):
     groups = copy.copy(chunk.camera_groups)
 
     groups.append(None)
@@ -135,13 +98,15 @@ def estimate_rotation_matrices(chunk, i, j):
             if c.reference.rotation is None:
                 if c.reference.location is None or next_camera.reference.location is None:
                     continue
-                direction = delta_vector_to_chunk(c.reference.location, next_camera.reference.location)
 
-                cos_yaw = direction * j
-                yaw = math.degrees(math.acos(cos_yaw)) + 90  # TODO not sure about this offset
+                prev_location = chunk.crs.unproject(c.reference.location)
+                next_location = chunk.crs.unproject(next_camera.reference.location)
 
-                if direction * i > 0:
-                    yaw = -yaw
+                direction = chunk.crs.localframe(prev_location).mulv(next_location - prev_location)
+
+                yaw = math.degrees(math.atan2(direction.y, direction.x)) + 90
+                if yaw < 0:
+                    yaw = yaw + 360
 
                 c.reference.rotation = ps.Vector([yaw, 0, 0])
 
@@ -149,14 +114,10 @@ def estimate_rotation_matrices(chunk, i, j):
 
 
 @time_measure
-def align_cameras(chunk, min_latitude, min_longitude):
-    if chunk.transform.scale is None:
-        chunk.transform.scale = 1
-        chunk.transform.rotation = ps.Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-        chunk.transform.translation = ps.Vector([0, 0, 0])
+def align_cameras(chunk):
+    init_chunk_transform(chunk)
 
-    i, j, k = get_chunk_vectors(min_latitude, min_longitude)  # i || North
-    estimate_rotation_matrices(chunk, i, j)
+    estimate_rotation_matrices(chunk)
 
     for c in chunk.cameras:
         if c.transform is not None:
@@ -165,32 +126,18 @@ def align_cameras(chunk, min_latitude, min_longitude):
         location = c.reference.location
         if location is None:
             continue
-        chunk_coordinates = wgs_to_chunk(chunk, location)
-        fi = c.reference.rotation.x + 90
-        fi = math.radians(fi)
-        roll = math.radians(c.reference.rotation.z)
-        pitch = math.radians(c.reference.rotation.y)
 
-        roll_mat = ps.Matrix([[1,              0,               0],
-                              [0, math.cos(roll), -math.sin(roll)],
-                              [0, math.sin(roll), math.cos(roll)]])
-        pitch_mat = ps.Matrix([[ math.cos(pitch), 0, math.sin(pitch)],
-                               [               0, 1,               0],
-                               [-math.sin(pitch), 0, math.cos(pitch)]])
-        yaw_mat = ps.Matrix([[math.cos(fi), -math.sin(fi), 0],
-                             [math.sin(fi),  math.cos(fi), 0],
-                             [           0,             0, 1]])
+        rotation = c.reference.rotation
+        if rotation is None:
+            continue
 
-        r = roll_mat * pitch_mat * yaw_mat
+        location = chunk.crs.unproject(location)  # location in ECEF
+        rotation = chunk.crs.localframe(location).rotation().t() * ps.Utils.euler2mat(rotation, chunk.euler_angles) # rotation matrix in ECEF
 
-        ii = r[0, 0] * i + r[1, 0] * j + r[2, 0] * k
-        jj = r[0, 1] * i + r[1, 1] * j + r[2, 1] * k
-        kk = r[0, 2] * i + r[1, 2] * j + r[2, 2] * k
+        transform = ps.Matrix.Translation(location) * ps.Matrix.Rotation(rotation)
+        transform = chunk.transform.matrix.inv() * transform * get_antenna_transform(c.sensor).inv()
 
-        c.transform = ps.Matrix([[ii.x, jj.x, kk.x, chunk_coordinates[0]],
-                                 [ii.y, jj.y, kk.y, chunk_coordinates[1]],
-                                 [ii.z, jj.z, kk.z, chunk_coordinates[2]],
-                                 [   0,    0,    0,                    1]])
+        c.transform = ps.Matrix.Translation(transform.translation()) * ps.Matrix.Rotation(transform.rotation())
 
 
 def run_camera_alignment():
@@ -202,9 +149,8 @@ def run_camera_alignment():
     if not check_chunk(chunk):
         return
 
-    min_latitude, min_longitude, max_latitude, max_longitude = get_chunk_bounds(chunk)
     try:
-        align_cameras(chunk, min_latitude, min_longitude)
+        align_cameras(chunk)
     except Exception as e:
         print(e)
 
