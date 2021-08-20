a33e62e3d71de8436c80e526df9535ad2ea6293e
Nikolay Polyarniy <polarnick@agisoft.com>
2021-08-20 16:28:42 +0300

    samples: general_worflow.py, network_processing.py

---
+++ src/samples/general_workflow.py
@@ -0,0 +1,77 @@
+import Metashape
+import os, sys, time
+
+# Checking compatibility
+compatible_major_version = "1.7"
+found_major_version = ".".join(Metashape.app.version.split('.')[:2])
+if found_major_version != compatible_major_version:
+    raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
+
+def find_files(folder, types):
+    return [entry.path for entry in os.scandir(folder) if (entry.is_file() and os.path.splitext(entry.name)[1].lower() in types)]
+
+if len(sys.argv) < 3:
+    print("Usage: general_workflow.py <image_folder> <output_folder>")
+    sys.exit(1)
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+photos = find_files(image_folder, [".jpg", ".jpeg", ".tif", ".tiff"])
+
+doc = Metashape.Document()
+doc.save(output_folder + '/project.psx')
+
+chunk = doc.addChunk()
+
+chunk.addPhotos(photos)
+doc.save()
+
+print(str(len(chunk.cameras)) + " images loaded")
+
+chunk.matchPhotos(keypoint_limit = 40000, tiepoint_limit = 10000, generic_preselection = True, reference_preselection = True)
+doc.save()
+
+chunk.alignCameras()
+doc.save()
+
+chunk.buildDepthMaps(downscale = 2, filter_mode = Metashape.MildFiltering)
+doc.save()
+
+chunk.buildModel(source_data = Metashape.DepthMapsData)
+doc.save()
+
+chunk.buildUV(page_count = 2, texture_size = 4096)
+doc.save()
+
+chunk.buildTexture(texture_size = 4096, ghosting_filter = True)
+doc.save()
+
+has_transform = chunk.transform.scale and chunk.transform.rotation and chunk.transform.translation
+
+if has_transform:
+    chunk.buildDenseCloud()
+    doc.save()
+
+    chunk.buildDem(source_data=Metashape.DenseCloudData)
+    doc.save()
+
+    chunk.buildOrthomosaic(surface_data=Metashape.ElevationData)
+    doc.save()
+
+# export results
+chunk.exportReport(output_folder + '/report.pdf')
+
+if chunk.model:
+    chunk.exportModel(output_folder + '/model.obj')
+
+if chunk.dense_cloud:
+    chunk.exportPoints(output_folder + '/dense_cloud.las', source_data = Metashape.DenseCloudData)
+
+if chunk.elevation:
+    chunk.exportRaster(output_folder + '/dem.tif', source_data = Metashape.ElevationData)
+
+if chunk.orthomosaic:
+    chunk.exportRaster(output_folder + '/orthomosaic.tif', source_data = Metashape.OrthomosaicData)
+
+print('Processing finished, results saved to ' + output_folder + '.')
---
+++ src/samples/network_processing.py
@@ -0,0 +1,130 @@
+import Metashape
+import os, sys, time
+
+# Checking compatibility
+compatible_major_version = "1.7"
+found_major_version = ".".join(Metashape.app.version.split('.')[:2])
+if found_major_version != compatible_major_version:
+    raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
+
+def find_files(folder, types):
+    return [entry.path for entry in os.scandir(folder) if (entry.is_file() and os.path.splitext(entry.name)[1].lower() in types)]
+
+if len(sys.argv) < 3:
+    print("Usage: general_workflow.py <image_folder> <output_folder>")
+    sys.exit(1)
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+network_server = '127.0.0.1'
+process_network = True
+
+Metashape.app.settings.network_path = '/mnt/datasets'
+
+photos = find_files(image_folder, [".jpg", ".jpeg", ".tif", ".tiff"])
+
+doc = Metashape.Document()
+doc.save(output_folder + '/project.psx')
+
+chunk = doc.addChunk()
+
+chunk.addPhotos(photos)
+doc.save()
+
+print(str(len(chunk.cameras)) + " images loaded")
+
+has_reference = False
+for c in chunk.cameras:
+    if c.reference.location:
+        has_reference = True
+
+tasks = []
+
+task = Metashape.Tasks.MatchPhotos()
+task.keypoint_limit = 40000
+task.tiepoint_limit = 10000
+task.generic_preselection = True
+task.reference_preselection = True
+tasks.append(task)
+
+task = Metashape.Tasks.AlignCameras()
+tasks.append(task)
+
+task = Metashape.Tasks.BuildDepthMaps()
+task.downscale = 2
+task.filter_mode = Metashape.MildFiltering
+tasks.append(task)
+
+task = Metashape.Tasks.BuildModel()
+task.source_data = Metashape.DepthMapsData
+tasks.append(task)
+
+task = Metashape.Tasks.BuildUV()
+task.page_count = 2
+task.texture_size = 4096
+tasks.append(task)
+
+task = Metashape.Tasks.BuildTexture()
+task.texture_size = 4096
+task.ghosting_filter = True
+tasks.append(task)
+
+if has_reference:
+    task = Metashape.Tasks.BuildDenseCloud()
+    tasks.append(task)
+
+    task = Metashape.Tasks.BuildDem()
+    task.source_data = Metashape.DenseCloudData
+    tasks.append(task)
+
+    task = Metashape.Tasks.BuildOrthomosaic()
+    task.surface_data = Metashape.ElevationData
+    tasks.append(task)
+
+task = Metashape.Tasks.ExportReport()
+task.path = output_folder + '/report.pdf'
+tasks.append(task)
+
+task = Metashape.Tasks.ExportModel()
+task.path = output_folder + '/model.obj'
+tasks.append(task)
+
+task = Metashape.Tasks.ExportPoints()
+task.path = output_folder + '/dense_cloud.las'
+task.source_data = Metashape.DenseCloudData
+tasks.append(task)
+
+if has_reference:
+    task = Metashape.Tasks.ExportRaster()
+    task.path = output_folder + '/dem.tif'
+    task.source_data = Metashape.ElevationData
+    tasks.append(task)
+
+    task = Metashape.Tasks.ExportRaster()
+    task.path = output_folder + '/orthomosaic.tif'
+    task.source_data = Metashape.OrthomosaicData
+    tasks.append(task)
+
+if process_network:
+    network_tasks = []
+    for task in tasks:
+        if task.target == Metashape.Tasks.DocumentTarget:
+            network_tasks.append(task.toNetworkTask(doc))
+        else:
+            network_tasks.append(task.toNetworkTask(chunk))
+
+    client = Metashape.NetworkClient()
+    client.connect(network_server)
+    batch_id = client.createBatch(doc.path, network_tasks)
+    client.resumeBatch(batch_id)
+
+    print('Processing started, results will be saved to ' + output_folder + '.')
+else:
+    for task in tasks:
+        if task.target == Metashape.Tasks.DocumentTarget:
+            task.apply(doc)
+        else:
+            task.apply(chunk)
+
+    print('Processing finished, results saved to ' + output_folder + '.')
