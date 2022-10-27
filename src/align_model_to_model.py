35c4eb06b9ebdef98a23504faee5e92435491bc9
Egor Vinokhodov <vineg@agisoft.com>
2022-10-27 16:09:33 +0300

    align_model_to_model.py: replaced - with -- in pip_install parameter

--- src/align_model_to_model.py
+++ src/align_model_to_model.py
@@ -26,7 +26,7 @@ found_major_version = ".".join(Metashape.app.version.split('.')[:2])
 if found_major_version != compatible_major_version:
     raise Exception("Incompatible Metashape version: {} != {}".format(found_major_version, compatible_major_version))
 
-pip_install("""—find-links https://vineg.github.io/python-wheels/wheels.html 
+pip_install("""--find-links https://vineg.github.io/python-wheels/wheels.html 
 open3d == 0.16.0
 pyhull == 2015.2.1""");
 
