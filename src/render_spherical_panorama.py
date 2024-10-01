322c71c8a10d77102d54199562bc7a39aa670b15
George Vinokhodov <vineg@agisoft.com>
2024-10-01 16:15:04 +0300

    render_spherical_panorama.py: point cloud support, use localframe

--- src/render_spherical_panorama.py
+++ src/render_spherical_panorama.py
@@ -7,6 +7,8 @@ import Metashape
 
 view_consistent_direction = False
 result_height_px = 4000
+source_data = Metashape.DataSource.ModelData
+# source_data = Metashape.DataSource.PointCloudData
 
 # Checking compatibility
 compatible_major_version = "2.1"
@@ -21,7 +23,6 @@ def render_spherical_panorama(result_height_px, center, rotation, result_path):
     """
 
     chunk = Metashape.app.document.chunk
-    model = chunk.model
     result_width_px = 2 * result_height_px
     y_up_z_back = Metashape.Matrix.Scale([1.0, -1.0, -1.0])
 
@@ -31,7 +32,12 @@ def render_spherical_panorama(result_height_px, center, rotation, result_path):
     calibration.height = result_height_px 
     calibration.type = Metashape.Sensor.Type.Spherical
 
-    model.renderImage((chunk.transform.matrix.inv() * Metashape.Matrix.Translation(center) * Metashape.Matrix.Rotation(rotation) * y_up_z_back), calibration).save(result_path)
+    transform = chunk.transform.matrix.inv() * Metashape.Matrix.Translation(center) * Metashape.Matrix.Rotation(rotation) * y_up_z_back
+
+    if (source_data == Metashape.DataSource.ModelData):
+        chunk.model.renderImage(transform, calibration).save(result_path)
+    elif (source_data == Metashape.DataSource.PointCloudData):
+        chunk.point_cloud.renderImage(transform, calibration, point_size=4).save(result_path)
 
 def render_panorama_from_current_point():
 
@@ -39,12 +45,16 @@ def render_panorama_from_current_point():
     if (chunk == None):
         raise Exception("Null chunk")
 
-    model = chunk.model
-    if (model == None):
-        raise Exception("Null model")
+    if (source_data == Metashape.DataSource.ModelData):
+        model = chunk.model
+        if (model == None):
+            raise Exception("Null model")
+        if (model.getActiveTexture() == None):
+            raise Exception("Model has no texture")
 
-    if (model.getActiveTexture() == None):
-        raise Exception("Model has no texture")
+    if (source_data == Metashape.DataSource.PointCloudData):
+        if (chunk.point_cloud == None):
+            raise Exception("Null point cloud")
 
     result_path = Metashape.app.getSaveFileName("Resulting render path", filter="All Images (*.tif *.jpg *.png);;TIFF (*.tif);;JPEG (*.jpg);;PNG (*.png);;All Formats(*.*)");
     if (result_path == ""):
@@ -55,11 +65,12 @@ def render_panorama_from_current_point():
     view_center = viewpoint.center
 
     # z up, x front, y left
-    z_up_rotation = Metashape.Matrix([
+    z_up_rotation = chunk.crs.localframe(view_center).rotation().t() * Metashape.Matrix([
         [0,  0, -1],
         [-1, 0, 0 ],
         [0,  1, 0 ]
     ])
+
     # panorama from viewpoint direction
     view_rotation = viewpoint.rot
 
